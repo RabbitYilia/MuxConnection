aa2f4137e2d698cfa773c192beb32fce5bfee3e0
Rabbit Yilia <takatsuki@outlook.com>
2018-09-20 15:28:43 +0800

    Add Process Function

--- Client/Client.go
+++ Client/Client.go
@@ -5,6 +5,7 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"encoding/json"
+	"hash"
 	"log"
 	"math/rand"
 	"net"
@@ -14,7 +15,18 @@ import (
 	"time"
 )
 
+var md5Ctx hash.Hash
+var packetbuffer map[string][]string
+var packettimestamp map[string]string
+var packetcount map[string]int
+var packettotal map[string]int
+
 func main() {
+	md5Ctx = md5.New()
+	packetbuffer = make(map[string][]string)
+	packettimestamp = make(map[string]string)
+	packetcount = make(map[string]int)
+	packettotal = make(map[string]int)
 
 	conn, err := net.Dial("tcp", "127.0.0.1:6161")
 	if err != nil {
@@ -24,9 +36,7 @@ func main() {
 	writer := bufio.NewWriter(conn)
 	//reader := bufio.NewReader(conn)
 
-	md5Ctx := md5.New()
 	for {
-		timestamp := strconv.FormatInt(time.Now().UnixNano(), 10)
 		log.Println("Please input msg:")
 		inputReader := bufio.NewReader(os.Stdin)
 		input, err := inputReader.ReadString('\n')
@@ -38,42 +48,112 @@ func main() {
 		if input == "" {
 			break
 		}
-		msg := hex.EncodeToString([]byte(input))
-		md5Ctx.Write([]byte(msg))
-		md5sum := hex.EncodeToString(md5Ctx.Sum(nil))
-		md5Ctx.Reset()
-		splitedmsgs := make(map[string]string)
-		piece := 0
-		log.Println(msg)
-		for {
-			piece += 1
-			if len(msg) < 10 {
-				splitedmsgs[strconv.Itoa(piece)] = msg
-				break
-			}
-			thislen := RandInt(1, len(msg))
-			splitedmsgs[strconv.Itoa(piece)] = msg[:thislen]
-			msg = msg[thislen:]
-		}
-		for thispiece, piecedmsg := range splitedmsgs {
-			var SendData = make(map[string]string)
-			SendData["Piece"] = strconv.Itoa(piece)
-			SendData["thisPiece"] = thispiece
-			SendData["timestamp"] = timestamp
-			SendData["md5sum"] = md5sum
-			SendData["piecedmsg"] = piecedmsg
-			SendJson, err := json.Marshal(SendData)
-			if err != nil {
-				log.Fatal(err)
-			}
-			writer.Write([]byte(SendJson))
-			writer.Write([]byte("\n"))
-			writer.Flush()
-		}
+		ProcessTXByte([]byte(input), writer)
 	}
 }
 
 func RandInt(min, max int) int {
 	rand.Seed(time.Now().UnixNano() * rand.Int63n(100))
 	return min + rand.Intn(max-min+1)
+}
+
+func ProcessTXByte(TXByte []byte, writer *bufio.Writer) {
+	timestamp := strconv.FormatInt(time.Now().UnixNano(), 10)
+	msg := hex.EncodeToString(TXByte)
+	md5Ctx.Write([]byte(msg))
+	md5sum := hex.EncodeToString(md5Ctx.Sum(nil))
+	md5Ctx.Reset()
+	splitedmsgs := make(map[string]string)
+	piece := 0
+	log.Println(msg)
+	for {
+		piece += 1
+		if len(msg) < 10 {
+			splitedmsgs[strconv.Itoa(piece)] = msg
+			break
+		}
+		thislen := RandInt(1, len(msg))
+		splitedmsgs[strconv.Itoa(piece)] = msg[:thislen]
+		msg = msg[thislen:]
+	}
+	for thispiece, piecedmsg := range splitedmsgs {
+		var SendData = make(map[string]string)
+		SendData["Piece"] = strconv.Itoa(piece)
+		SendData["thisPiece"] = thispiece
+		SendData["timestamp"] = timestamp
+		SendData["md5sum"] = md5sum
+		SendData["piecedmsg"] = piecedmsg
+		SendJson, err := json.Marshal(SendData)
+		if err != nil {
+			log.Fatal(err)
+		}
+		writer.Write([]byte(SendJson))
+		writer.Write([]byte("\n"))
+		writer.Flush()
+	}
+}
+
+func ProcessRXByte(RXByte []byte) {
+	RXdata := make(map[string]string)
+	err := json.Unmarshal(RXByte, &RXdata)
+	if err != nil {
+		return
+	}
+	databuffer, ok := packetbuffer[RXdata["md5sum"]]
+	if !ok {
+		packettimestamp[RXdata["md5sum"]] = RXdata["timestamp"]
+		thistotal, err := strconv.Atoi(RXdata["Piece"])
+		if err != nil {
+			return
+		}
+		packetint, err := strconv.Atoi(RXdata["thisPiece"])
+		if err != nil {
+			return
+		}
+
+		var thisbuffer []string
+		thisbuffer = make([]string, thistotal+1)
+		packetbuffer[RXdata["md5sum"]] = thisbuffer
+		packetcount[RXdata["md5sum"]] = 1
+		packettotal[RXdata["md5sum"]] = thistotal
+		log.Println("OK")
+		packetbuffer[RXdata["md5sum"]][packetint] = RXdata["piecedmsg"]
+		if packetcount[RXdata["md5sum"]] == packettotal[RXdata["md5sum"]] {
+			DataStr := ""
+			for i := 1; i <= packettotal[RXdata["md5sum"]]; i++ {
+				DataStr += packetbuffer[RXdata["md5sum"]][i]
+			}
+			ByteData, err := hex.DecodeString(DataStr)
+			if err != nil {
+				return
+			}
+			log.Println(string(ByteData))
+			delete(packetbuffer, RXdata["md5sum"])
+			delete(packettimestamp, RXdata["md5sum"])
+			delete(packetcount, RXdata["md5sum"])
+			delete(packettotal, RXdata["md5sum"])
+		}
+	} else {
+		packetint, err := strconv.Atoi(RXdata["thisPiece"])
+		if err != nil {
+			return
+		}
+		databuffer[packetint] = RXdata["piecedmsg"]
+		packetcount[RXdata["md5sum"]] += 1
+		if packetcount[RXdata["md5sum"]] == packettotal[RXdata["md5sum"]] {
+			DataStr := ""
+			for i := 1; i <= packettotal[RXdata["md5sum"]]; i++ {
+				DataStr += packetbuffer[RXdata["md5sum"]][i]
+			}
+			ByteData, err := hex.DecodeString(DataStr)
+			if err != nil {
+				return
+			}
+			log.Println(string(ByteData))
+			delete(packetbuffer, RXdata["md5sum"])
+			delete(packettimestamp, RXdata["md5sum"])
+			delete(packetcount, RXdata["md5sum"])
+			delete(packettotal, RXdata["md5sum"])
+		}
+	}
 }
--- Server/Server.go
+++ Server/Server.go
@@ -2,11 +2,29 @@ package main
 
 import (
 	"bufio"
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"hash"
 	"log"
+	"math/rand"
 	"net"
+	"strconv"
+	"time"
 )
 
+var packetbuffer map[string][]string
+var packettimestamp map[string]string
+var packetcount map[string]int
+var packettotal map[string]int
+var md5Ctx hash.Hash
+
 func main() {
+	md5Ctx = md5.New()
+	packetbuffer = make(map[string][]string)
+	packettimestamp = make(map[string]string)
+	packetcount = make(map[string]int)
+	packettotal = make(map[string]int)
 	server, err := net.Listen("tcp", "127.0.0.1:6161")
 	if err != nil {
 		log.Fatal(err)
@@ -17,7 +35,7 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		go handleRequest(conn)
+		handleRequest(conn)
 	}
 }
 
@@ -29,12 +47,119 @@ func handleRequest(conn net.Conn) {
 	writer := bufio.NewWriter(conn)
 
 	for {
-		b, _, err := reader.ReadLine()
-		log.Println(b)
+		RXByte, _, err := reader.ReadLine()
 		if err != nil {
 			return
 		}
+		ProcessRXByte(RXByte)
+
 		writer.Write([]byte("\n"))
 		writer.Flush()
 	}
+}
+
+func ProcessTXByte(TXByte []byte, writer *bufio.Writer) {
+	timestamp := strconv.FormatInt(time.Now().UnixNano(), 10)
+	msg := hex.EncodeToString(TXByte)
+	md5Ctx.Write([]byte(msg))
+	md5sum := hex.EncodeToString(md5Ctx.Sum(nil))
+	md5Ctx.Reset()
+	splitedmsgs := make(map[string]string)
+	piece := 0
+	log.Println(msg)
+	for {
+		piece += 1
+		if len(msg) < 10 {
+			splitedmsgs[strconv.Itoa(piece)] = msg
+			break
+		}
+		thislen := RandInt(1, len(msg))
+		splitedmsgs[strconv.Itoa(piece)] = msg[:thislen]
+		msg = msg[thislen:]
+	}
+	for thispiece, piecedmsg := range splitedmsgs {
+		var SendData = make(map[string]string)
+		SendData["Piece"] = strconv.Itoa(piece)
+		SendData["thisPiece"] = thispiece
+		SendData["timestamp"] = timestamp
+		SendData["md5sum"] = md5sum
+		SendData["piecedmsg"] = piecedmsg
+		SendJson, err := json.Marshal(SendData)
+		if err != nil {
+			log.Fatal(err)
+		}
+		writer.Write([]byte(SendJson))
+		writer.Write([]byte("\n"))
+		writer.Flush()
+	}
+}
+
+func ProcessRXByte(RXByte []byte) {
+	RXdata := make(map[string]string)
+	err := json.Unmarshal(RXByte, &RXdata)
+	if err != nil {
+		return
+	}
+	databuffer, ok := packetbuffer[RXdata["md5sum"]]
+	if !ok {
+		packettimestamp[RXdata["md5sum"]] = RXdata["timestamp"]
+		thistotal, err := strconv.Atoi(RXdata["Piece"])
+		if err != nil {
+			return
+		}
+		packetint, err := strconv.Atoi(RXdata["thisPiece"])
+		if err != nil {
+			return
+		}
+
+		var thisbuffer []string
+		thisbuffer = make([]string, thistotal+1)
+		packetbuffer[RXdata["md5sum"]] = thisbuffer
+		packetcount[RXdata["md5sum"]] = 1
+		packettotal[RXdata["md5sum"]] = thistotal
+		log.Println("OK")
+		packetbuffer[RXdata["md5sum"]][packetint] = RXdata["piecedmsg"]
+		if packetcount[RXdata["md5sum"]] == packettotal[RXdata["md5sum"]] {
+			DataStr := ""
+			for i := 1; i <= packettotal[RXdata["md5sum"]]; i++ {
+				DataStr += packetbuffer[RXdata["md5sum"]][i]
+			}
+			ByteData, err := hex.DecodeString(DataStr)
+			if err != nil {
+				return
+			}
+			log.Println(string(ByteData))
+			delete(packetbuffer, RXdata["md5sum"])
+			delete(packettimestamp, RXdata["md5sum"])
+			delete(packetcount, RXdata["md5sum"])
+			delete(packettotal, RXdata["md5sum"])
+		}
+	} else {
+		packetint, err := strconv.Atoi(RXdata["thisPiece"])
+		if err != nil {
+			return
+		}
+		databuffer[packetint] = RXdata["piecedmsg"]
+		packetcount[RXdata["md5sum"]] += 1
+		if packetcount[RXdata["md5sum"]] == packettotal[RXdata["md5sum"]] {
+			DataStr := ""
+			for i := 1; i <= packettotal[RXdata["md5sum"]]; i++ {
+				DataStr += packetbuffer[RXdata["md5sum"]][i]
+			}
+			ByteData, err := hex.DecodeString(DataStr)
+			if err != nil {
+				return
+			}
+			log.Println(string(ByteData))
+			delete(packetbuffer, RXdata["md5sum"])
+			delete(packettimestamp, RXdata["md5sum"])
+			delete(packetcount, RXdata["md5sum"])
+			delete(packettotal, RXdata["md5sum"])
+		}
+	}
+}
+
+func RandInt(min, max int) int {
+	rand.Seed(time.Now().UnixNano() * rand.Int63n(100))
+	return min + rand.Intn(max-min+1)
 }
